fea8b44df482c8c18dc976bba4292f3f48e85159
zhangxutong <zhangxutong@imcoming.cn>
2016-12-12 15:18:37 +0800

    线性布局Demo完善 更诱人

---
+++ app/src/main/java/mcxtzhang/commonviewgroupadapter/linearlayout/K50Bean.java
@@ -0,0 +1,26 @@
+package mcxtzhang.commonviewgroupadapter.linearlayout;
+
+/**
+ * 介绍：50K特供bean
+ * 作者：zhangxutong
+ * 邮箱：mcxtzhang@163.com
+ * 主页：http://blog.csdn.net/zxt0601
+ * 时间： 2016/12/12.
+ */
+
+public class K50Bean {
+    private String name;
+
+    public K50Bean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public K50Bean setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
--- app/src/main/java/mcxtzhang/commonviewgroupadapter/linearlayout/LinearLayoutActivity.java
+++ app/src/main/java/mcxtzhang/commonviewgroupadapter/linearlayout/LinearLayoutActivity.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.mcxtzhang.commonadapter.viewgroup.ViewGroupUtils;
 import com.mcxtzhang.commonadapter.viewgroup.adapter.single.SingleAdapter;
 
@@ -16,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import mcxtzhang.commonviewgroupadapter.R;
-import mcxtzhang.commonviewgroupadapter.TestBean;
+
 /**
  * 介绍：单一Item类型：
  * 作者：zhangxutong
@@ -26,40 +24,53 @@ import mcxtzhang.commonviewgroupadapter.TestBean;
  */
 public class LinearLayoutActivity extends AppCompatActivity {
     private static final String TAG = "zxt";
-    private List<TestBean> mDatas;
-    private LinearLayout mLinearLayout;
+    private List<K50Bean> mDatas;
+    private LinearLayout llCurrent, llUseMore, llRecent;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
-        mLinearLayout = (LinearLayout) findViewById(R.id.activity_linear_layout);
+        llCurrent = (LinearLayout) findViewById(R.id.llCurrent);
+        llUseMore = (LinearLayout) findViewById(R.id.llUseMore);
+        llRecent = (LinearLayout) findViewById(R.id.llRecent);
         mDatas = initDatas();
 
         //单一ItemView
-        ViewGroupUtils.addViews(mLinearLayout, new SingleAdapter<TestBean>(this, mDatas, R.layout.item_test) {
+        ViewGroupUtils.addViews(llCurrent, new SingleAdapter<K50Bean>(this, mDatas, R.layout.item_50k) {
             @Override
-            public void onBindView(ViewGroup parent, View itemView, TestBean data, int pos) {
-                Glide.with(LinearLayoutActivity.this)
-                        .load(data.getAvatar())
-                        .into((ImageView) itemView.findViewById(R.id.ivAvatar));
-                ((TextView) itemView.findViewById(R.id.tvName)).setText(data.getName());
+            public void onBindView(ViewGroup parent, View itemView, K50Bean data, int pos) {
+                ((TextView) itemView.findViewById(R.id.tv)).setText(data.getName());
+            }
+        });
+
+        ViewGroupUtils.addViews(llUseMore, new SingleAdapter<K50Bean>(this, mDatas, R.layout.item_50k) {
+            @Override
+            public void onBindView(ViewGroup parent, View itemView, K50Bean data, int pos) {
+                ((TextView) itemView.findViewById(R.id.tv)).setText(data.getName());
+            }
+        });
+
+        ViewGroupUtils.addViews(llRecent, new SingleAdapter<K50Bean>(this, mDatas, R.layout.item_50k) {
+            @Override
+            public void onBindView(ViewGroup parent, View itemView, K50Bean data, int pos) {
+                ((TextView) itemView.findViewById(R.id.tv)).setText(data.getName());
             }
         });
 
     }
 
-    public List<TestBean> initDatas() {
-        List<TestBean> datas = new ArrayList<>();
-        datas.add(new TestBean("http://imgs.ebrun.com/resources/2016_03/2016_03_25/201603259771458878793312_origin.jpg", "张"));
-        datas.add(new TestBean("http://p14.go007.com/2014_11_02_05/a03541088cce31b8_1.jpg", "旭童"));
-        datas.add(new TestBean("http://news.k618.cn/tech/201604/W020160407281077548026.jpg", "多种type"));
-        datas.add(new TestBean("http://www.kejik.com/image/1460343965520.jpg", "多种type"));
-        datas.add(new TestBean("http://cn.chinadaily.com.cn/img/attachement/jpg/site1/20160318/eca86bd77be61855f1b81c.jpg", "多种type"));
-        datas.add(new TestBean("http://imgs.ebrun.com/resources/2016_04/2016_04_12/201604124411460430531500.jpg", "多种type"));
-        datas.add(new TestBean("http://imgs.ebrun.com/resources/2016_04/2016_04_24/201604244971461460826484_origin.jpeg", "多种type"));
-        datas.add(new TestBean("http://www.lnmoto.cn/bbs/data/attachment/forum/201408/12/074018gshshia3is1cw3sg.jpg", "多种type"));
+    public List<K50Bean> initDatas() {
+        List<K50Bean> datas = new ArrayList<>();
+        datas.add(new K50Bean("自行车"));
+        datas.add(new K50Bean("自行车"));
+        datas.add(new K50Bean("自行车"));
+        datas.add(new K50Bean("自行车"));
+        datas.add(new K50Bean("自行车"));
+        datas.add(new K50Bean("自行车"));
+        datas.add(new K50Bean("自行车"));
+        datas.add(new K50Bean("自行车"));
         return datas;
     }
 }
