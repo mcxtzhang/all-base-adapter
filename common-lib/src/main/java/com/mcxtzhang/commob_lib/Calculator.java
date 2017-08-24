c1ed4a5c22a39b86031fee860c45b1217ffcae84
mcxtzhang@163.com <sunwentao@imcoming.cn>
2017-08-24 22:21:51 +0800

    【base】单元测试

---
+++ common-lib/src/main/java/com/mcxtzhang/commob_lib/Calculator.java
@@ -0,0 +1,21 @@
+package com.mcxtzhang.commob_lib;
+
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a+b;
+    }
+
+    public double substract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+}
+
---
+++ common-lib/src/test/java/com/mcxtzhang/commob_lib/CalculatorTest.java
@@ -0,0 +1,45 @@
+package com.mcxtzhang.commob_lib;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * 介绍：
+ * 作者：zhangxutong
+ * 邮箱：mcxtzhang@163.com
+ * 主页：http://blog.csdn.net/zxt0601
+ * 时间： 2017/8/24.
+ */
+public class CalculatorTest {
+    private Calculator mCalculator;
+
+    @Before
+    public void setUp() throws Exception {
+        mCalculator = new Calculator();
+    }
+
+    @Test
+    public void testSum() throws Exception {
+        //expected: 6, sum of 1 and 5
+        assertEquals(6d, mCalculator.sum(1d, 5d), 0);
+    }
+
+    @Test
+    public void testSubstract() throws Exception {
+        assertEquals(1d, mCalculator.substract(5d, 4d), 0);
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        assertEquals(4d, mCalculator.divide(20d, 5d), 0);
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        assertEquals(10d, mCalculator.multiply(2d, 5d), 0);
+    }
+
+
+}
